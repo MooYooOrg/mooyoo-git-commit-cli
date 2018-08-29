e8858764ab83c330183c51a4aa7eec41889f3df2
quinlanzhong <quinlanzhong@meijiabang.cn>
2018-08-29 15:13:11 +0800

    fix: 当不选择 scope 的时候，应该过滤 no scope

--- dest/helpers/combineTypeScope.js
+++ dest/helpers/combineTypeScope.js
@@ -7,9 +7,10 @@ var combineTypeScope = function combineTypeScope(type, scope) {
   var thisType = type;
 
   var thisScope = scope || '';
+  var excludeScopes = ['(none)', 'no scope'];
 
   // add scope correctly if ':' is at the end
-  if (thisScope.length > 0 && thisScope !== '(None)' && thisScope !== '(none)') {
+  if (thisScope.length > 0 && !excludeScopes.includes(thisScope.toLowerCase())) {
     if (thisType.charAt(thisType.length - 1) === ':') {
       thisType = thisType.slice(0, thisType.length - 1);
       thisType = '' + thisType + thisScope.toLowerCase() + ':';
